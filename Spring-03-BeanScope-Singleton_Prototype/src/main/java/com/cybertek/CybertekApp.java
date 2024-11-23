e4d9e5a3c6c35fcb572f1e4e89e801dfd6bc9354
Alex2055 <alexfromnashville@gmail.com>
2024-11-22 20:23:01 -0600

    Bean Life Cycle

--- Spring-03-BeanScope-Singleton_Prototype/src/main/java/com/cybertek/CybertekApp.java
+++ Spring-03-BeanScope-Singleton_Prototype/src/main/java/com/cybertek/CybertekApp.java
@@ -7,9 +7,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class CybertekApp {
     public static void main(String[] args) {
         ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
-        Course course = container.getBean("java", Course.class);
-        course.getTeachingHours();
+       //prototype
+        Course course1 = container.getBean("java", Course.class);
+        Course course2 = container.getBean("java", Course.class);
+        System.out.println("Pointing to the same object: "+ (course1==course2));
+        System.out.println("Memory Location for the course1: "+course1);
+        System.out.println("Memory Location for the course2: "+course2);
+
+        //singleton
+        Course course3 = container.getBean("selenium", Course.class);
+        Course course4 = container.getBean("selenium", Course.class);
+        System.out.println("Pointing to the same object: "+ (course3==course4));
+        System.out.println("Memory Location for the course3: "+course3);
+        System.out.println("Memory Location for the course4: "+course4);
+
     }
 }
-
-
