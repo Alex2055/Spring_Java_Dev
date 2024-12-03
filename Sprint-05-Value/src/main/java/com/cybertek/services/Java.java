da6328c8e3eff4c258e3bba56d436a02d474b6d3
Alex2055 <alexfromnashville@gmail.com>
2024-12-03 13:20:20 -0600

    05 Value

---
+++ Sprint-05-Value/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,47 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class Java implements Course {
+@Value("JD1")
+ private String batch;
+@Value("${instructor}")
+ private String instructor;
+@Value("${days}")
+ private String[] days;
+
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "days=" + Arrays.toString(days) +
+                ", batch='" + batch + '\'' +
+                ", instructor='" + instructor + '\'' +
+                '}';
+    }
+
+    //@Autowired
+//@Qualifier("mockInterviewHours")
+    private ExtraSessions extraSessions;
+
+
+    public Java(@Qualifier("mockInterviewHours") ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours: "+(20 + extraSessions.getHours()))
+    ;}
+
+
+}
+
